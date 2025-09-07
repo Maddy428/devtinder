d9d9a6bc143edaf9d919e03189b257e8879022b2
Madan Mohan Reddy Somagattu <madanmohanreddysomagattu@Madans-MacBook-Air.local>
2025-09-07 09:37:56 +0530

    curd route and http

---
+++ homework.md
@@ -0,0 +1,17 @@
+- Create a repository 
+- Initialize the repository 
+- node_modules,package.json, package-lock.json
+- install express
+- create a server
+- listen to port 7777
+- Write request handlers for /test, /hello
+- Install nodemon and update script inside package.json 
+- What are dependenciess
+- What is the use of "-g" while npm install
+- Difference between caret and tilde(^ vs ~)
+
+
+- initialize git 
+- gitignore
+- Create a remote repo on github
+- push all code to remote origin 
--- src/app.js
+++ src/app.js
@@ -2,19 +2,45 @@ const express = require('express');
 
 const app = express();
 
+app.get("/users",(req,res)=>{
+    console.log(req.query);
+    res.send({firstName:"Madan",lastName: "Reddy"})
+})
 
+app.get("/users/:userid/:name/:password",(req,res)=>{
+    console.log(req.params);
+    res.send({firstName:"Madan",lastName: "Reddy"})
+})
 
-app.use('/test',(req,res)=>{
-    res.send("testing test request handlers !!!! comingS")
+app.get("/users/:id",(req,res)=>{
+    console.log(req.params);
+    res.send({firstName:"Madan",lastName: "Reddy"})
+})
+
+app.post("/users",(req,res)=>{
+    // Save data into DB 
+    res.send("Data saved successfully!!!")
+})
+
+app.patch("/users",(req,res)=>{
+    // Save data into DB 
+    res.send("Data updated successfully!!!")
+})
+
+app.delete("/users",(req,res)=>{
+    // Save data into DB 
+    res.send("Data delted successfully!!!")
+})
+
+app.get("/ab*cd",(req,res)=>{
+    res.send({firstName:"Madan",lastName: "Reddy"})
 })
 
 app.use('/hello',(req,res)=>{
-    res.send("testing hello request handlers !!!! data")
+    res.send("testing test request handlers !!!! comingS")
 })
 
-app.use('/',(req,res)=>{
-    res.send("hello world here! !!")
-})
+
 
 app.listen(7777, ()=>{
     console.log("port is listening 7777.....")
